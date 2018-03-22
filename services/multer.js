db5d4e2545f1af0efd1082a574899f16d8048c7b
Amir <amir.lavi2@gmail.com>
2018-03-22 20:03:59 +0200

    Update multer.js

--- services/multer.js
+++ services/multer.js
@@ -5,7 +5,7 @@ const storageCfg = multer.diskStorage({
     destination: (req, file, cb)=>{cb(null, './upload/')}, // file will be here
     filename: (req, file, cb) =>{cb(null, file.originalname)} // save file the with the original name
 });
-const maxSize = { fileSize: 1024 * 1024 * 1024 * 5}; // max upload of 5 MB
+const maxSize = { fileSize: 1024 * 1024 * 5}; // max upload of 5 MB
 
 // upload middleware
 const upload = multer({storage: storageCfg, limits: maxSize});
